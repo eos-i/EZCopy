5b6e62d6300e1d02718e15317cc38fb331718f63
wangyousheng <wangyousheng@fanletech.com>
2022-06-02 19:08:47 +0800

    change:update code

--- app/src/main/java/com/eos/ezcopy/activity/MainActivity.java
+++ app/src/main/java/com/eos/ezcopy/activity/MainActivity.java
@@ -59,7 +59,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         initEvent();
-        initData();
+//        initData();
         initAdapter();
     }
 
